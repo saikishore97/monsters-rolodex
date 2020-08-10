313393d6c4df5428f320a6b15bd9766d2de9f438
saikishore97 <saikishore.kssk@gmail.com>
2020-08-10 17:38:59 -0500

    Updates

---
+++ precache-manifest.edba904e2a424a7fcafe502807c39661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b32eb86e1cdc5fc5d091fa2d24fd5f1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "05903f9504a0eaf6b2a6",
+    "url": "/monsters-rolodex/static/css/main.2fb3f87b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05903f9504a0eaf6b2a6",
+    "url": "/monsters-rolodex/static/js/main.68741aee.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
